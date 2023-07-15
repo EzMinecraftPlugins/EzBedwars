5b2f6bb4271731bf7101d2a76ebd4bbfb512638f
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-15 21:18:03 +1000

    Update Loader.php

--- src/Loader.php
+++ src/Loader.php
@@ -10,9 +10,27 @@ class Loader extends PluginBase {
     final function onEnable(): void
     {
 final function onCommand(CommandSender $sender, Command $command, string $value, array $args): bool {
-switch ($command->getName()) {
-            case "bedwars":
-    $this->BedwarsCommand();
-}
+if (isset($args[0])) {
+            switch ($args[0]) {
+                case 'setup':
+                if ($sender->hasPermission("EzBedwars.setup"){
+                    
+return true;
+                    }
+                    else {
+$sender->sendMessage("Error: No Perm");
+                    return false;
+}  
+                case 'setting':
+                case 'settings':
+                case 'edit':
+                
+                    break;
+                    
+                default:
+                    $sender->sendMessage("§cUsage: /bedwars (Subcommand)");
+                    break;
+            }
+        }
     }
 }
