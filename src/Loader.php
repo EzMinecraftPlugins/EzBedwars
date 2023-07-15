7383d0b3ed5c1d5fbf8c8225a38390879d6071e8
Erfan <113671298+ErfanAzadi@users.noreply.github.com>
2023-07-15 14:16:36 +0330

    Add files via upload

---
+++ src/Loader.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EzMinecraftPlugins\EzBedwars;
+
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase {
+    final function onEnable(): void
+    {
+        # register command /bedwars
+    }
+}
---
+++ src/commands/BedwarsCommand.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace EzMinecraftPlugins\EzBedwars\commands;
+
+use pocketmine\command\Command;
+
+final class BedwarsCommand extends Command {
+
+}
